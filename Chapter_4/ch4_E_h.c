b33f0408d4de1d5aff502aabb8278f74b74ec6ca
ankitmahe <ankitmahe1234@gmail.com>
2023-05-30 18:47:36 +0530

    update

--- Chapter_4/ch4_E_h.c
+++ Chapter_4/ch4_E_h.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     float sal;
     printf("Enter the Salary = ");
-    scanf("%f",&sal);
-    ((sal>=25000) && (sal<=40000))? printf("Manager\n") : ((sal>=15000) && (sal<=25000)) ? printf("Accountant\n"):printf("Clerk");
+    scanf("%f", &sal);
+    ((sal >= 25000) && (sal <= 40000)) ? printf("Manager\n") : ((sal >= 15000) && (sal <= 25000)) ? printf("Accountant\n")
+    : printf("Clerk");
     return 0;
 }
