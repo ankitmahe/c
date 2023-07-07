3007b92ff6aa093004f784373cdc0cfcfe3985be
ankitmahe <ankitmahe1234@gmail.com>
2023-07-07 08:54:24 +0530

    update

--- Chapter_6/ch_6_C_o.c
+++ Chapter_6/ch_6_C_o.c
@@ -1,18 +1,38 @@
-#include<stdio.h>
-int main()
-{
-    int i,j,k;
-    for(i=1;i<=5;i++)
-    {
-        for(j=5;j>=i;j--)
-        {
-            printf("  ");
-        }
-        for(k=1;k<=i*2-1;k++)
-        {
-            printf("* ");
-        }
-        printf("\n");
-    }
-    return 0;
+// #include<stdio.h>
+// int main()
+// {
+//     int i,j,k;
+//     for(i=1;i<=5;i++)
+//     {
+//         for(j=5;j>=i;j--)
+//         {
+//             printf("  ");
+//         }
+//         for(k=1;k<=i;k++)
+//         {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+#include <stdio.h>
+int main() {
+   int coef = 1, k, i, j;
+   for (i = 0; i < 5; i++) 
+   {
+      for (j = 1; j <= 5 - i; j++)
+         printf("  ");
+      for (k = 0; k <= i; k++) {
+         if (k == 0 || i == 0)
+            coef = 1;
+         else
+            coef = coef * (i - k + 1) / k;
+         printf("%4d", coef);
+      }
+      printf("\n");
+   }
+   return 0;
 }
